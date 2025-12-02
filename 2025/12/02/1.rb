1ec89d72a82adac7bd64cf8f2ac2b233a08b89f6
Suhas <jargnar@gmail.com>
2025-12-02 11:26:07 +0530

    update 2025 day 02 to use regex solution for both

--- 2025/12/02/1.rb
+++ 2025/12/02/1.rb
@@ -1,8 +1,6 @@
 puts $<.read.split(',').sum { |r|
   a, b = r.split('-').map(&:to_i)
   (a..b).sum { |n|
-    s = n.to_s
-    s.size.even? && s[0, s.size/2] * 2 == s ? n : 0
+    n.to_s =~ /^(.+)\1$/ ? n : 0
   }
 }
-
